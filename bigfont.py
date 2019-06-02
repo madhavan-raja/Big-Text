6b1b45b73be9a8bbbb4fa81e6bb15376e3fa7043
Madhavan <madhavanraja99@gmail.com>
2019-06-02 12:06:15 +0530

    Update bigfont.py

--- bigfont.py
+++ bigfont.py
@@ -1,13 +1,13 @@
-from characters_3w import Font
+from characters_3w import Palette
 
 
 def display(msg, hor_scale=1, ver_scale=1, space_char=' ', true_char='█', false_char=' '):
-    char_height = Font.height
+    char_height = Palette.height
 
     for h in range(char_height):
         for x in range(ver_scale):
             for ch in msg:
-                for line in Font.chars.get(ch)[h]:
+                for line in Palette.chars.get(ch)[h]:
                     for big_chr in line:
                         for y in range(hor_scale):
                             if big_chr == ' ' or big_chr == '0':
