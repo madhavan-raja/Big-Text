a00950f81c851d369de7c2c6aea28d2faacb9933
Madhavan <madhavanraja99@gmail.com>
2019-06-04 21:57:56 +0530

    Removed Palette class and added absent characters

--- bigtext.py
+++ bigtext.py
@@ -1,16 +1,16 @@
-from characters_3w import Palette
+from characters_3w import *
 
 
-def display(msg, hor_scale=1, ver_scale=1, space_char=' ', true_char='█', false_char=' '):
-    char_height = Palette.height
-
-    for h in range(char_height):
+def display(msg, hor_scale=1, ver_scale=1, space_char=' ', true_char='█', false_char=' ', absent_char='!'):
+    for h in range(height):
         for x in range(ver_scale):
             for ch in msg:
-                for line in Palette.chars.get(ch)[h]:
+                for line in chars.get(ch, [['!']] * height)[h]:
                     for big_chr in line:
                         for y in range(hor_scale):
-                            if big_chr == ' ' or big_chr == '0':
+                            if big_chr == '!':
+                                print(absent_char, end='')
+                            elif big_chr == ' ' or big_chr == '0':
                                 print(false_char, end='')
                             else:
                                 print(true_char, end='')
